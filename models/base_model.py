34799725e89f5e57379c8d8a13a8fa1010955111
sumutoni <54947406+sumutoni@users.noreply.github.com>
2023-04-16 17:35:52 +0200

    added declarative base

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,9 @@ from datetime import datetime
 from sqlalchemy import Column, Integer, String, Table, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
+
+
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), nullable=False, primary_key=True)
