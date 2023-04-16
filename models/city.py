e2d6f4e16bd0a70b29984de9473eecc201346618
sumutoni <54947406+sumutoni@users.noreply.github.com>
2023-04-16 17:34:57 +0200

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, Integer, String, Table, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 
-
-class City(BaseModel):
+Base = declarative_base()
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     state_id = ""
     name = ""
