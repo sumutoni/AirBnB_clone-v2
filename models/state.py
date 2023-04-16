ef234324712c28a9e2c574faf5fe2aa8a5ede49c
sumutoni <54947406+sumutoni@users.noreply.github.com>
2023-04-16 17:43:37 +0200

    added column and reference to table

--- models/state.py
+++ models/state.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base, Column, String
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
