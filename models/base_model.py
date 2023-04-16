165cee566cd02c0e3e05b734d2058ee5e61008a8
sumutoni <steumutoni@gmail.com>
2023-04-16 09:41:21 -0700

    fixed style issues

--- models/base_model.py
+++ models/base_model.py
@@ -13,7 +13,7 @@ class BaseModel:
     id = Column(String(60), nullable=False, primary_key=True)
     created_at = Column(DateTime, nullable=False, default=datetime.now())
     updated_at = Column(DateTime, nullable=False, default=datetime.now())
-    
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
@@ -59,6 +59,7 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
+
     def delete(self):
         """Delete object from storage"""
         storage.delete(self)
