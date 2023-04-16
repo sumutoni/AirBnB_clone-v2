aa2f7d8bb850656bd640f7a5e741a791cab3d3df
sumutoni <steumutoni@gmail.com>
2023-04-16 09:50:24 -0700

    Fixed style issues

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -13,52 +13,53 @@ from models.user import User
 
 
 Class DBStorage():
-  """DBStorage Class definition"""
-  __engine = None
-  __session = None
-  
-  
-  def __init__(self):
-    """Initialize engine"""
-    self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
-      getenv('HBNB_MYSQL_USER'), getenv('HBNB_MYSQL_PWD'),
-      getenv('HBNB_MYSQL_HOST'), getenv('HBNB_MYSQL_DB')), pool_pre_ping=True)
-    self.__classes = {"User": User, "Amenity": Amenity, "City": City,
-                      "Place": Place, "Review": Review, "State": State}
-    self.__class = ["User", "Amenity", "City", "Place", "Review", "State"]
-    if getenv('HBNB_ENV') == 'test':
-      Base.metadat.drop_all(self.__engine)
+    """DBStorage Class definition"""
+    __engine = None
+    __session = None
 
-  def all(self, cls=None):
-    """return objects based value of cls, all objects otherwise"""
-    objs = {}
-    if cls is None:
-      for cl in self.__class:
-        ls_cl = session.query(cl).all()
-        if ls_cl:
-          for c in ls_cl:
-            objs[c.__class__.name + '.' + c.id] = c
-    else:
-      ls_cl = session.query(cls).all()
-      if ls_cl:
-          for c in ls_cl:
-            objs[c.__class__.name + '.' + c.id] = c
-    return objs
+    def __init__(self):
+        """Initialize engine"""
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+            getenv('HBNB_MYSQL_USER'), getenv('HBNB_MYSQL_PWD'),
+            getenv('HBNB_MYSQL_HOST'), getenv('HBNB_MYSQL_DB')),
+            pool_pre_ping=True)
+        self.__classes = {"User": User, "Amenity": Amenity, "City": City,
+                          "Place": Place, "Review": Review, "State": State}
+        self.__class = ["User", "Amenity", "City", "Place", "Review", "State"]
+        if getenv('HBNB_ENV') == 'test':
+            Base.metadat.drop_all(self.__engine)
 
-  def new(self):
-    """add object to current session"""
-    self.__session.add(self)
+    def all(self, cls=None):
+        """return objects based value of cls, all objects otherwise"""
+        objs = {}
+        if cls is None:
+            for cl in self.__class:
+                ls_cl = session.query(cl).all()
+                if ls_cl:
+                    for c in ls_cl:
+                        objs[c.__class__.name + '.' + c.id] = c
+        else:
+            ls_cl = session.query(cls).all()
+            if ls_cl:
+                for c in ls_cl:
+                    objs[c.__class__.name + '.' + c.id] = c
+        return objs
 
-  def save(self):
-    """commit all changes in current session"""
-    self.__session.commit()
+    def new(self):
+        """add object to current session"""
+        self.__session.add(self)
 
-  def delete(self, obj=None):
-    """delete object from current session if not None"""
-    if obj is not None:
-      self.__session.delete(obj)
+    def save(self):
+        """commit all changes in current session"""
+        self.__session.commit()
 
-  def reload(self):
-    """creating session"""
-    Base.metadata.create_all(self.__engine)
-    self.__session = scoped_session(sessionmaker(bind=self.__engine, expire_on_commit=False))
+    def delete(self, obj=None):
+        """delete object from current session if not None"""
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """creating session"""
+        Base.metadata.create_all(self.__engine)
+        self.__session = scoped_session(sessionmaker(bind=self.__engine,
+                                        expire_on_commit=False))
