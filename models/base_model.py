a58c31bfa1ce23f673910a362a719a8919700a78
sumutoni <54947406+sumutoni@users.noreply.github.com>
2023-04-16 19:44:59 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -58,7 +58,7 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        print(dictionary)
+        dictionary.pop('_sa_instance_state', None)
         return dictionary
 
     def delete(self):
