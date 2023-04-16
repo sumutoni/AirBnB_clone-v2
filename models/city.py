1c5c6b30310c52042121541bcbceed2e0f76f44b
sumutoni <54947406+sumutoni@users.noreply.github.com>
2023-04-16 17:39:46 +0200

    added Columns and reference to table

--- models/city.py
+++ models/city.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
-from sqlalchemy import Column, Integer, String, Table, DateTime
+from sqlalchemy import Column, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import ForeignKey
 
 Base = declarative_base()
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), nullable=False, ForeignKey('states.id'))
